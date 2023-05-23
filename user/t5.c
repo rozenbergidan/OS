3236b4f1ef11e75d3ad27e4ac1442e5757a7474e
Edan Levy <62389779+EdanLevy@users.noreply.github.com>
2023-05-23 10:44:50 +0000

    works with small numbers

--- user/t5.c
+++ user/t5.c
@@ -4,11 +4,11 @@
 void main()
 {
     printf("ustack, world!\n");
-    int *a = (int *)ustack_malloc(8);
+    int *a = (int *)ustack_malloc(200);
     printf("a = %d\n", *a);
     *a = 5;
     printf("a = %d\n", *a);
-    int *b = (int *)ustack_malloc(8);
+    int *b = (int *)ustack_malloc(512);
     printf("b = %d\n", *b);
     *b = 6;
     printf("b = %d\n", *b);
@@ -17,7 +17,7 @@ void main()
     printf("freed memory = %d\n", freed_memory);
     printf("b = %d\n", *b);
 
-    int *c = (int *)ustack_malloc(4);
+    int *c = (int *)ustack_malloc(1000);
     printf("c = %d\n", *c);
     *c = 7;
     printf("c = %d\n", *c);
